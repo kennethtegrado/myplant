da1dbdbf5304ef3e24b8900ddf0cccbae56f625f
Kenneth Tegrado <tegradok@gmail.com>
2022-07-21 22:49:39 +0800

    feat: add util function for fetching data from sanity studio
    Create config.ts to store configurations of the Sanity project for
    establishing connection between the sanity studio and the next
    application.
    
    Create util function for building url of files uploaded on the CMS and
    fetching other data function from the studio.

---
+++ utils/sanity/index.ts
@@ -0,0 +1,30 @@
+import { createClient, createPreviewSubscriptionHook } from 'next-sanity';
+
+import createImageUrlBuilder from '@sanity/image-url';
+
+import { config } from './config';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+
+if (!config.projectId) {
+    throw Error('The Project ID is not set. Check your environment variables.');
+}
+
+const builder = createImageUrlBuilder(config);
+
+export const urlFor = (source: SanityImageSource) => builder.image(source);
+
+export const imageBuilder = (source: SanityImageSource) =>
+    builder.image(source);
+
+export const usePreviewSubscription = createPreviewSubscriptionHook(config);
+
+export const client = createClient(config);
+
+export const previewClient = createClient({
+    ...config,
+    useCdn: false,
+});
+
+export const getClient = (usePreview?: boolean) =>
+    usePreview ? previewClient : client;
+export default client;
